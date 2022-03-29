ee85943ed688acc6a849deb7b70128ca4c0c3707
samar mehri <samar.mehri@git.assistants.epita.fr>
2022-03-30 01:53:11 +0200

    Partie 2

---
+++ src/main/java/fr/lernejo/navy_battle/Server.java
@@ -0,0 +1,24 @@
+package fr.lernejo.navy_battle;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+public class Server {
+
+    private final int port;
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    public void serverInit() throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(this.port), 0);
+        server.setExecutor(Executors.newFixedThreadPool(1));
+        server.createContext("/ping", new Ping());
+        server.start();
+    }
+}
+

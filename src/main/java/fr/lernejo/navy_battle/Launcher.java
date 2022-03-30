76c438a8dbae0b62c4b778facc706a74a41b8c58
samar mehri <samar.mehri@git.assistants.epita.fr>
2022-03-30 02:10:47 +0200

    Launcher

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,7 +1,22 @@
 package fr.lernejo.navy_battle;
 
+import java.io.IOException;
+
 public class Launcher {
     public static void main(String[] args) {
+        int port = Integer.parseInt(args[0]);
+        try {
+            System.out.println("Server start at port " + port);
+            new Server().start(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        if (args.length < 1) {
+            System.out.println("Entrez un port");
+            return;
+        }
 
+        int port = Integer.parseInt(args[0]);
+        Server server = new Server(port);
+        server.start();
     }
-}
+

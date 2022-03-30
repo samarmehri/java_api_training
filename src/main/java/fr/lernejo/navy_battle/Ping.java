a5fdc010b5d6d7952251a5e90f4e0ee890ed7150
samar mehri <samar.mehri@git.assistants.epita.fr>
2022-03-30 17:12:03 +0200

    update ping

--- src/main/java/fr/lernejo/navy_battle/Ping.java
+++ src/main/java/fr/lernejo/navy_battle/Ping.java
@@ -1,18 +1,16 @@
 package fr.lernejo.navy_battle;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+
 import java.io.IOException;
 import java.io.OutputStream;
-
 public class Ping implements HttpHandler {
-
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String body = "OK";
         exchange.sendResponseHeaders(200, body.length());
-        try (OutputStream os = exchange.getResponseBody()) { 
+        try (OutputStream os = exchange.getResponseBody()) { // (1)
             os.write(body.getBytes());
         }
-        exchange.close();
     }
 }

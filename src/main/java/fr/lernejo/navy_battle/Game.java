44ca5295426a39fafb79cdca05174a387da5e402
samar mehri <samar.mehri@git.assistants.epita.fr>
2022-03-30 17:38:15 +0200

    try to fix bug

--- src/main/java/fr/lernejo/navy_battle/Game.java
+++ src/main/java/fr/lernejo/navy_battle/Game.java
@@ -1,49 +1,26 @@
 package fr.lernejo.navy_battle;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.util.Map;
 
-public class Game implements HttpHandler {
+public class Game implements HttpHandler
+{
     private final Map<String, String> gameSetting;
-
     public Game(Map<String, String> gameInfo) {
         this.gameSetting = gameInfo;
     }
-
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        System.out.println(exchange.getRequestMethod());
+        ObjectMapper objectMapper = new ObjectMapper(); System.out.println(exchange.getRequestMethod());
         if (exchange.getRequestMethod().equals("POST")) {
-            try {
-                InputStream response = exchange.getRequestBody();
-                objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-                Request requestReceive = objectMapper.readValue(response, Request.class);
-                String messageRequest = requestReceive.getMessage();
-                gameSetting.put("client_id", requestReceive.getId());
-                gameSetting.put("client_url", requestReceive.getUrl());
-
-                System.out.println(messageRequest);
-                System.out.println(gameSetting.get("id"));
-                System.out.println(gameSetting.get("port"));
-                System.out.println(gameSetting.get("client_id"));
-                System.out.println(gameSetting.get("client_url"));
-            }catch (IOException e) {
-                exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
-            }
-            Request requestSend = new Request(gameSetting.get("id"), "http://localhost:" + gameSetting.get("port"), "May the best code win");
-            String messageSend = objectMapper.writeValueAsString(requestSend);
-
-            exchange.sendResponseHeaders(HttpURLConnection.HTTP_ACCEPTED, messageSend.getBytes().length);
-            exchange.getResponseBody().write(messageSend.getBytes());
-        }
-        else {
-            exchange.sendResponseHeaders(HttpURLConnection.HTTP_NOT_FOUND, 0);
-        }
+            try {InputStream response = exchange.getRequestBody(); objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false); Request requestReceive = objectMapper.readValue(response, Request
+                .class); String messageRequest = requestReceive.getMessage(); gameSetting.put("client_id", requestReceive.getId());gameSetting.put("client_url", requestReceive.getUrl());System.out.println(messageRequest);System.out.println(gameSetting.get("id"));System.out.println(gameSetting.get("port"));System.out.println(gameSetting.get("client_id"));System.out.println(gameSetting.get("client_url"));}catch (IOException e) {exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);}
+            Request requestSend = new Request(gameSetting.get("id"), "http://localhost:" + gameSetting.get("port"), "May the best code win");String messageSend = objectMapper.writeValueAsString(requestSend);exchange.sendResponseHeaders(HttpURLConnection.HTTP_ACCEPTED, messageSend.getBytes().length);exchange.getResponseBody().write(messageSend.getBytes());}
+        else {exchange.sendResponseHeaders(HttpURLConnection.HTTP_NOT_FOUND, 0);}
     }
 }
--- src/main/java/fr/lernejo/navy_battle/Server.java
+++ src/main/java/fr/lernejo/navy_battle/Server.java
@@ -4,27 +4,19 @@ import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.util.concurrent.Executors;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-
-public class Server {
-    private final int port;
-    private final Map<String, String> gameInfo;
-    public Server(int port, Client client, Map<String, String> gameInfo) {
-
-        this.gameInfo = gameInfo;
-        this.port = port;
-    }
-
-    public void start() throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(this.port), 0);
-        Game serverInitGame = new Game(gameInfo);
-
-        server.setExecutor(Executors.newFixedThreadPool(1));
-        server.createContext("/ping", new Ping());
-	 server.createContext("/api/game/game", serverInitGame);
-        server.start();
+public class Server
+{
+    private final Map<String, String> gameSetting;private final Client client;private final int port;
+    public Server(int port, Client client, Map<String, String> gameSetting)
+    {this.gameSetting = gameSetting;this.port = port;this.client = client;}
+    public void start()
+    {
+        InetSocketAddress socketAddr = new InetSocketAddress(port);ExecutorService executorService = Executors.newFixedThreadPool(1);
+        try {HttpServer httpServer = HttpServer.create(socketAddr, 0);Game Game= new Game(gameSetting);System.out.println("Server start at port : " + port);httpServer.setExecutor(executorService);httpServer.createContext("/ping", new Ping());httpServer.createContext("/api/game/start", Game);httpServer.start();
+        } catch (IOException e) {e.printStackTrace();}
     }
 }
-

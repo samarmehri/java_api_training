e860c4a2dca2894588e49f3357e933aacaf0e037
samar mehri <samar.mehri@git.assistants.epita.fr>
2022-03-30 02:37:07 +0200

    fix Launcher

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,22 +1,26 @@
 package fr.lernejo.navy_battle;
-
+import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.*;
 
-public class Launcher {
-    public static void main(String[] args) {
-        int port = Integer.parseInt(args[0]);
-        try {
-            System.out.println("Server start at port " + port);
-            new Server().start(port);
-        } catch (IOException e) {
-            e.printStackTrace();
-        if (args.length < 1) {
-            System.out.println("Entrez un port");
-            return;
-        }
 
-        int port = Integer.parseInt(args[0]);
-        Server server = new Server(port);
-        server.start();
+public class Launcher
+{
+    private static void startServer(int serverPort) throws IOException
+    {
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress(serverPort), 0);
+        httpServer.setExecutor(Executors.newSingleThreadExecutor());
+        httpServer.createContext("/ping", new Ping());
+        httpServer.start();
     }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0)
+            return;
+        int serverPort = Integer.parseInt(args[0]);
+        System.out.println("Listening on port :" + serverPort);
+        startServer(serverPort);
+    }
+}
 
